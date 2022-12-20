247b0417347fd15755926b0d9f03bcf4a0a9fe6a
sampathkumarpullipudi <119720342+sampathkumarpullipudi@users.noreply.github.com>
2022-12-20 20:24:36 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n==1)     {         printf("monday");     }     else if(n==2)     {         printf("tuesday");     }     else     {         printf("invalid input");     } }

---
+++ Day_name__.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==1)
+    {
+        printf("monday");
+    }
+    else if(n==2)
+    {
+        printf("tuesday");
+    }
+    else 
+    {
+        printf("invalid input");
+    }
+}
